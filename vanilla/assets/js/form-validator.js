5495a06c9c29eb4f2308e1bab513abef9411d029
Wilfrido Bonett <wbonett10@gmail.com>
2020-07-24 00:28:49 -0500

    Validatiosn form is working

--- vanilla/assets/js/form-validator.js
+++ vanilla/assets/js/form-validator.js
@@ -1,9 +1,7 @@
 function customInputValidation() {
 
     var validateElements = document.getElementsByClassName("validate");
-
-    console.log(validateElements)
-
+    var isChecked = termsConditionsChecked();
     var inputs = Array.prototype.filter.call(validateElements,
         (element) => {
             return element.nodeName === 'INPUT';
@@ -19,8 +17,16 @@ function customInputValidation() {
         }
 
         if (input.value.length < 1 && input.id === 'pnumber') {
-            input.placeholder = "Enter a valid phone number";
+            input.placeholder = "Enter value email";
             input.classList.add("input--error");
         }
     }
+
+    if (isChecked) {
+        alert('sent');
+    }
+}
+
+function termsConditionsChecked(input) {
+    return document.getElementById('accept-terms').checked;
 }
