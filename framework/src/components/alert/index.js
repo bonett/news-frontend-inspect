51f924244469d4ce566c6c20a00cdd4a3a621df9
Wilfrido Bonett <wbonett10@gmail.com>
2020-07-29 17:40:23 -0500

    root files in progress

---
+++ framework/src/components/alert/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './style.scss';
+
+const Alert = () => {
+  return (
+    <h3>Alert</h3>
+  );
+}
+
+Alert.propTypes = {
+
+}
+
+export default Alert;
