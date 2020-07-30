8f63a67c240ed04032629f06459c67892fc42aec
Wilfrido Bonett <wbonett10@gmail.com>
2020-07-30 10:47:13 -0500

    navigation bar is working

--- framework/src/components/navigation/index.js
+++ framework/src/components/navigation/index.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 import Avatar from 'emerald-ui/lib/Avatar';
 import DropdownButton from 'emerald-ui/lib/DropdownButton';
 import DropdownItem from 'emerald-ui/lib/DropdownItem';
-import Icon from 'emerald-ui/lib/Icon';
 import Nav from 'emerald-ui/lib/Nav';
 import Navbar from 'emerald-ui/lib/Navbar';
 
@@ -15,39 +14,32 @@ const Navigation = () => {
     const logoImage = require('../../assets/img/logo.svg');
     
     return (
-        <Navbar breakAt="sm" barClassName="container">
-            <Navbar.Brand>
+        <Navbar breakAt="md" barClassName="container" className="navbar__content">
+            <Navbar.Brand className="navbar__logo">
                 <a href="#foo">
                     <img src={logoImage} alt="News" />
                 </a>
             </Navbar.Brand>
-            <Nav grow collapsible>
-                <DropdownButton title="Sections" id="dd1">
-                    <DropdownItem eventKey="1">Action</DropdownItem>
-                    <DropdownItem eventKey="2">Another action</DropdownItem>
-                    <DropdownItem eventKey="3" active>
-                        Active Item
-            </DropdownItem>
-                    <DropdownItem separator />
-                    <DropdownItem eventKey="4">Separated link</DropdownItem>
+            <Nav grow collapsible className="navbar__menu">
+                <DropdownButton title="Sections" id="dropdown-menu" className="navbar__menu__item">
+                    <DropdownItem eventKey="1"><i className="fa fa-motorcycle"></i> Sports</DropdownItem>
+                    <DropdownItem eventKey="2"><i className="fa fa-home"></i>Entertainment</DropdownItem>
+                    <DropdownItem eventKey="3"><i className="fa fa-btc"></i>Economics</DropdownItem>
+                    <DropdownItem eventKey="4"><i className="fa fa-laptop"></i>Technology</DropdownItem>
                 </DropdownButton>
-                <a href="#foo">Editorial</a>
-                <a href="#foo">Contact Us</a>
+                <a href="#foo" className="navbar__menu__item">Editorial</a>
+                <a href="#foo" className="navbar__menu__item">Contact Us</a>
             </Nav>
-            <Nav>
+            <Nav className="navbar__account">
                 <DropdownButton
                     noCaret
                     fromRight
-                    id="dd2"
+                    className="navbar__account__dropdown"
+                    id="dropdown-setting"
                     title={<Avatar title="JS" />}
                 >
-                    <DropdownItem eventKey="1">Action</DropdownItem>
-                    <DropdownItem eventKey="2">Another action</DropdownItem>
-                    <DropdownItem eventKey="3" active>
-                        Active Item
-            </DropdownItem>
-                    <DropdownItem separator />
-                    <DropdownItem eventKey="4">Separated link</DropdownItem>
+                    <DropdownItem eventKey="1">My Account</DropdownItem>
+                    <DropdownItem eventKey="2">Settings</DropdownItem>
                 </DropdownButton>
             </Nav>
 
